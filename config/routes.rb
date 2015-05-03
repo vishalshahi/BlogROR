782f4a057479a68c4f7a4215ee46bd79f9291624
Vishal Shahi <shahivishal09@gmail.com>
2015-05-03 16:36:12 +0530

    Iteration-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :comments
+ # resources :comments
 
   resources :posts do
-   resources:comments
+   resources :comments
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
